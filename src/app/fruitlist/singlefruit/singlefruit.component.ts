1ab13565c40c50d5d2e9212d92a591bdbd3f0023
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-11-17 19:44:22 +0100

    ngModel aka. "banana in a box"

--- src/app/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-singlefruit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './singlefruit.component.html',
   styleUrl: './singlefruit.component.scss'
 })
@@ -18,9 +19,13 @@ export class SinglefruitComponent {
     reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
   };
 
-  @Output()fruitname = new EventEmitter<string>();
+  inputData = "";
 
-  emitName() {
-    this.fruitname.emit(this.fruit.name);
+  @Output() fruitname = new EventEmitter<string>();
+
+  sendInputData() {
+    // this.fruitname.emit(this.fruit.name);
+    console.log(this.inputData);
+    // this.inputData = "Moin";
   }
 }
